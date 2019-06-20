8d7505c31a68bc9be2612cfdc66a9afffb540f3b
Anna Smirnova <anya94_pochta@mail.ru>
2019-06-21 02:24:10 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$(window).load(function() {
+	$('#main-slider').flexslider({
+	  animation: "slide",
+	  
+  });
+  
+  $('#reviews-slider').flexslider({
+	  animation: "slide",
+	  itemMargin: 50,
+
+  });
+  });
+  
+  
